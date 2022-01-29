10141a698a0d824c9b33d359d022e0cf0210e257
Susanna Yossef <susannayossef98@gmail.com>
2022-01-29 14:13:43 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './redux/store'
+import store from './redux/store';
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.querySelector('#root')
 );
-
-export default App;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
