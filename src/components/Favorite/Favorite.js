3ed5c112abf47cbfa5dc62aa66a7e65da0d88aee
Susanna Yossef <susannayossef98@gmail.com>
2022-02-05 14:12:36 +0100

    update

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -1,9 +1,25 @@
-import PageTitle from '../PageTitle/PageTitle'
+// import NavBar from '../NavBar/NavBar';
+import PageTitle from '../PageTitle/PageTitle';
+import styles from './Favorite.module.scss';
+import Card from '../Card/Card';
+import { useSelector } from 'react-redux';
+import { getFavorites } from '../../redux/cardsRedux';
+
 
 const Favorite = () => {
+
+  const cards = useSelector(state => getFavorites(state));
+
   return (
-    <PageTitle>Favorite</PageTitle>
-  );
+      <>
+        <PageTitle>FAVORITE</PageTitle>
+        <section className={styles.column}>
+          <ul className={styles.cards}>
+            {cards.map(card => <Card key={card.id} id={card.id} title={card.title} isFavorite={card.isFavorite}/>)}
+          </ul>
+        </section>        
+      </>
+  )
 };
 
 export default Favorite;
